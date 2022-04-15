6f5d437065ec77ae54bb7d14b297139e67b99d2f
EternalMarch <32583303+EternalMarch@users.noreply.github.com>
2022-04-15 10:55:31 +0200

    More types

--- serverTypes.ts
+++ serverTypes.ts
@@ -58,3 +58,20 @@ export interface UserFromDatabase {
 	__v: number,
 	workouts: WorkoutFromDatabase[]
 }
+
+export interface SuccessfulLoginData {
+	message: 'Login successful',
+	status: 200,
+	data: {
+		username: string,
+		internal_id: string,
+		socket: string | null,
+		latest_connect: string,
+		first_connection: string
+	}
+}
+
+export interface ErrorLoginData {
+	message: 'Wrong password or username',
+	status: 401,
+}
